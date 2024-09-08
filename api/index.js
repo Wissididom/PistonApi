cccbe7cee4c6c3edf518e67a8a91868484fedb9e
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-09-08 13:34:33 +0200

    Make handler async and format code

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 import helmet from "helmet";
 
@@ -11,70 +11,72 @@ app.use(express.urlencoded({ extended: false }));
 app.get(`/check`, (req, res) => {
   res.send("Server is running");
 });
-app.get(`*`, (req, res) => {
+app.get(`*`, async (req, res) => {
   if (!req.body.language) {
-  	res.status(400);
+    res.status(400);
     res.send("No language specified");
     return;
   }
   if (!req.body.code) {
-  	res.status(400);
+    res.status(400);
     res.send("No code specified");
     return;
   }
-  let runtimes = await fetch('https://emkc.org/api/v2/piston/runtimes').then(res => res.json());
+  let runtimes = await fetch("https://emkc.org/api/v2/piston/runtimes").then(
+    (res) => res.json(),
+  );
   let values = {
-  	language: req.body.language,
-  	version: 'N/A',
-  	code: req.body.code
-  }
+    language: req.body.language,
+    version: "N/A",
+    code: req.body.code,
+  };
   let foundRuntime = false;
   for (let runtime of runtimes) {
-  	let runtimeNames = [runtime.language, ...runtime.aliases];
-  	for (let runtimeName of runtimeNames) {
-  		if (runtimeName.toLowerCase() == req.body.language.toLowerCase()) {
-  			values.version = runtime.version;
-  			foundRuntime = true;
-  		}
-  	}
+    let runtimeNames = [runtime.language, ...runtime.aliases];
+    for (let runtimeName of runtimeNames) {
+      if (runtimeName.toLowerCase() == req.body.language.toLowerCase()) {
+        values.version = runtime.version;
+        foundRuntime = true;
+      }
+    }
   }
   if (!foundRuntime) {
-  	res.status(404);
+    res.status(404);
     res.send("Runtime not found");
     return;
   }
   let payload = {
-  	language: values.language,
-  	version: values.version,
-  	files: [
-  		{
-  			content: values.code
-  		}
-  	]
+    language: values.language,
+    version: values.version,
+    files: [
+      {
+        content: values.code,
+      },
+    ],
   };
-  let executeResult = await fetch('https://emkc.org/api/v2/piston/execute', {
-  	method: 'POST',
-  	body: JSON.stringify(payload)
-  }).then(res => res.json());
-  let response = '';
+  let executeResult = await fetch("https://emkc.org/api/v2/piston/execute", {
+    method: "POST",
+    body: JSON.stringify(payload),
+  }).then((res) => res.json());
+  let response = "";
   if (executeResult.compile) {
-  	if (executeResult.compile.code) {
-  		if (response.length <= 1) {
-  			response = `Compile-Output: (Code ${executeResult.compile.code}): ${executeResult.compile.output}`;
-  		} else {
-  			response = `; Compile-Output: (Code ${executeResult.compile.code}): ${executeResult.compile.output}`;
-  		}
-  	}
+    if (executeResult.compile.code) {
+      if (response.length <= 1) {
+        response = `Compile-Output: (Code ${executeResult.compile.code}): ${executeResult.compile.output}`;
+      } else {
+        response = `; Compile-Output: (Code ${executeResult.compile.code}): ${executeResult.compile.output}`;
+      }
+    }
   }
   if (executeResult.run) {
-  	if (response.length <= 1) {
-  	  			response = `Run-Output: (Code ${executeResult.run.code}): ${executeResult.run.output}`;
-  	  		} else {
-  	  			response = `; Run-Output: (Code ${executeResult.run.code}): ${executeResult.run.output}`;
-  	  		}
+    if (response.length <= 1) {
+      response = `Run-Output: (Code ${executeResult.run.code}): ${executeResult.run.output}`;
+    } else {
+      response = `; Run-Output: (Code ${executeResult.run.code}): ${executeResult.run.output}`;
+    }
   }
   if (response.length > 499) {
-  	respponse = response.substring(0, 499);
+    respponse = response.substring(0, 499);
   }
   res.send(response.trim());
 });
